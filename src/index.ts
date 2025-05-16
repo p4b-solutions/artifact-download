d5cb2968c30f3903ccfde739d994e9baeb7fc1ea
Nazar Vaskiv <vaskiv27@gmail.com>
2025-05-16 11:50:32 +0300

    - init

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import * as core from "@actions/core";
+import fs from "node:fs";
+import path from "node:path";
+import * as tar from "tar";
+import axios from "axios";
+
+async function run(): Promise<void> {
+  try {
+    const name = core.getInput("name");
+    const root = core.getInput("path");
+    const url = core.getInput("url");
+    const workflow_id = core.getInput("workflow_id");
+    const files = name.includes(" ") ? name.split(" ") : name.includes(",") ? name.split(",").map((it) => it.trim()) : [name];
+    console.log(`Download artifacts for workflow ${workflow_id}: `, files);
+    await Promise.all(
+      files.map(async (file) => {
+        const response = await axios.get(`${url}/${workflow_id}/${file}.tgz`, { responseType: "arraybuffer" });
+        const folder = path.join(root, file);
+        fs.mkdirSync(folder, { recursive: true });
+        const archive = path.join(folder, `${file}.tgz`);
+        fs.writeFileSync(archive, response.data);
+        await tar.extract({ file: archive, cwd: folder });
+        fs.unlinkSync(archive);
+      })
+    );
+  } catch (error: any) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
