124fa87e38ba2f9c7adcadc1366ba1acb3daca16
Nazar Vaskiv <vaskiv27@gmail.com>
2025-05-16 12:13:46 +0300

    - update

--- dist/index.js
+++ dist/index.js
@@ -29463,6 +29463,7 @@ async function run() {
         const root = core.getInput("path");
         const url = core.getInput("url");
         const workflow_id = core.getInput("workflow_id");
+        const cleanup = core.getInput("cleanup") === "true";
         const files = name.includes(" ") ? name.split(" ") : name.includes(",") ? name.split(",").map((it) => it.trim()) : [name];
         console.log(`Download artifacts for workflow ${workflow_id}: `, files);
         await Promise.all(files.map(async (file) => {
@@ -29474,6 +29475,13 @@ async function run() {
             await tar.extract({ file: archive, cwd: folder });
             node_fs_1.default.unlinkSync(archive);
         }));
+        try {
+            if (cleanup)
+                await axios_1.default.delete(`${url}/${workflow_id}`);
+        }
+        catch (error) {
+            console.log("Error deleting artifacts: ", error);
+        }
     }
     catch (error) {
         core.setFailed(error.message);
--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ async function run(): Promise<void> {
     const root = core.getInput("path");
     const url = core.getInput("url");
     const workflow_id = core.getInput("workflow_id");
+    const cleanup = core.getInput("cleanup") === "true";
     const files = name.includes(" ") ? name.split(" ") : name.includes(",") ? name.split(",").map((it) => it.trim()) : [name];
     console.log(`Download artifacts for workflow ${workflow_id}: `, files);
     await Promise.all(
@@ -23,6 +24,11 @@ async function run(): Promise<void> {
         fs.unlinkSync(archive);
       })
     );
+    try {
+      if (cleanup) await axios.delete(`${url}/${workflow_id}`);
+    } catch (error) {
+      console.log("Error deleting artifacts: ", error);
+    }
   } catch (error: any) {
     core.setFailed(error.message);
   }
